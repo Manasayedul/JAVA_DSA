8341ff8f6f82da2fbdb0344c3e3087bd3797c347
Manasayedul <manasayeddula25@gmail.com>
2026-03-11 12:50:12 +0530

    Add Thing class to count instances

---
+++ Day_10/Thing.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Thing{
+    static int count;
+    Thing(){
+        count++;
+    }
+    static void getCount(){
+        System.out.println(count);
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        Thing ob=new Thing();
+        new Thing();
+        new Thing();
+              
+        ob.getCount();
+    }
+}
